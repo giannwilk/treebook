156c161a7de4c72a9832f825e168e5b6e7814c44
Giann Wilkerson <giannwilk@gmail.com>
2013-02-13 10:47:44 -0500

    Began coding user log in testing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,12 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me,
                   :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
+  
+    validates :first_name, presence: true
+
+    validates :last_name, presence: true
+
+    validates :profile_name, presence: true
 
   has_many :statuses
 
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test" a user should enter a first name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:first_name].empty?
+end
+
+
+	test"a user should enter a last name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:last_name].empty?
+	end
+
+
+	test"a user should enter a profile name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+
+
+	test"a user should have a unique profie name" do
+		user = User.new
+		user.profile_name = 'giannwilk'
+		user.email = 'giannwilk@gmail.com'
+		user.first_name = 'Giann'
+		user.last_name = 'Wilkerson'
+		user.password = 'password'
+		user.password_confirmation ='password'
+		assert !user.save
+		puts user.errors.inspect
+		assert !user.errors[:profile_name].empty?
+	end
+
 end
