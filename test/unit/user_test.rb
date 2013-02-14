8b1b1f037c4b2e248a27561ca84b33be26140bc4
Giann Wilkerson <giannwilk@gmail.com>
2013-02-13 20:09:25 -0500

    Added and verified  user test. Known working state: Green.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,38 +1,38 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-	test" a user should enter a first name" do
-		user = User.new
-		assert !user.save
-		assert !user.errors[:first_name].empty?
-end
+  test "a user should enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
 
+  test "a user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
 
-	test"a user should enter a last name" do
-		user = User.new
-		assert !user.save
-		assert !user.errors[:last_name].empty?
-	end
+  test "a user should enter a profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
 
+  test "a user should have a unique profile name" do
+    user = User.new
+    user.profile_name = users(:giann).profile_name
+    
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
 
-	test"a user should enter a profile name" do
-		user = User.new
-		assert !user.save
-		assert !user.errors[:profile_name].empty?
-	end
+  test "a user should have a profile name without spaces" do
+    user = User.new
+    user.profile_name = "My Profile With Spaces"
 
-
-	test"a user should have a unique profie name" do
-		user = User.new
-		user.profile_name = 'giannwilk'
-		user.email = 'giannwilk@gmail.com'
-		user.first_name = 'Giann'
-		user.last_name = 'Wilkerson'
-		user.password = 'password'
-		user.password_confirmation ='password'
-		assert !user.save
-		puts user.errors.inspect
-		assert !user.errors[:profile_name].empty?
-	end
-
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
 end
