e11d647f6e0fc7dbf2bcad875fccd2432545fa84
Giann Wilkerson <giannwilk@gmail.com>
2013-03-01 16:24:29 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Treebook::Application.routes.draw do
 
   resources :statuses
   get 'feed', to: 'statuses#index', as: :feed
-  root to: 'statuses#index'
+  root to: 'devise/sessions#new'
 
   get '/:id', to: 'profiles#show'
 
