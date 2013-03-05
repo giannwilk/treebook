1abf068773f90d17607394be2ded09ffa792bd31
Giann Wilkerson <giannwilk@gmail.com>
2013-03-05 13:57:00 -0500

    Updated users and now working corretly

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -42,7 +42,7 @@ class StatusesController < ApplicationController
   # POST /statuses
   # POST /statuses.json
   def create
-    @status = Status.new(params[:status])
+    @status = current_user.statuses.new(params[:status])
 
     respond_to do |format|
       if @status.save
@@ -58,8 +58,8 @@ class StatusesController < ApplicationController
   # PUT /statuses/1
   # PUT /statuses/1.json
   def update
-    @status = Status.find(params[:id])
-
+    @status = current_user.statuses.find(params[:id])
+    params[:status].delete(:user_id) if params[:status].has_key?(:user_id)
     respond_to do |format|
       if @status.update_attributes(params[:status])
         format.html { redirect_to @status, notice: 'Status was successfully updated.' }
