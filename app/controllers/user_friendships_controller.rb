6e4c1efdc997a99362b1298b5c081ee8dc40651a
Giann Wilkerson <giannwilk@gmail.com>
2013-07-16 15:39:42 -0400

    Updated page, now working although still getting friend required message but not broken.

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -2,7 +2,7 @@ class UserFriendshipsController < ApplicationController
   before_filter :authenticate_user!, only: [:new]
 
   def new
-    if params[:friend_id]
+    if params[:user_friendship] && params[:user_friendship].has_key?(:friend_id)
       @friend = User.find(params[:friend_id])
       @user_friendship = current_user.user_friendships.new(friend: @friend)
     else
